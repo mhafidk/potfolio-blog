48701400a24f821967cce9b2d4da1b5eeb55ec5a
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-11-24 14:00:33 +0700

    add layout

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,10 @@
+<header class="bg-neutral-900 text-center text-neutral-100">
+	<nav class="mx-auto max-w-screen-md px-4 py-8">
+		<a href="/" class="text-xl font-bold">Muhammad <span class="text-lime-500">Hafid</span> K</a>
+		<div class="mt-4 flex justify-center gap-6 text-sm">
+			<a href="/about" class="hover:text-lime-500">About</a>
+			<a href="/blog" class="hover:text-lime-500">Blog</a>
+			<a href="/#contact" class="hover:text-lime-500">Contact</a>
+		</div>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
+	import Header from '$lib/components/header.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Header />
+<main class="bg-neutral-900 text-neutral-100">
+	<div class="mx-auto max-w-screen-md">
+		{@render children()}
+	</div>
+</main>
+<Footer />
