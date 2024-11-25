95450d1ef9e5cdceefa804d5ee8ec70b9fad7aa2
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-11-25 11:24:19 +0700

    add about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,4 +4,38 @@
 
 <div class="">
 	<h1 class="text-center text-4xl font-bold text-lime-500">About</h1>
+	<div class="mt-16">
+		<p>Hi, how are you? I hope you are fine!</p>
+		<p class="mt-4">
+			My name is <span class="font-medium text-lime-500">Muhammad Hafid Khoirul</span>, people
+			usually call me
+			<span class="font-medium text-lime-500">Hafid</span>.
+		</p>
+		<p class="mt-4">
+			I'm a software developer based in Bogor, Indonesia, currently working as a backend engineer at <a
+				class="border-b-2 border-lime-500 text-lime-500 hover:border-lime-400 hover:text-lime-400"
+				href="https://www.qontak.com"
+				target="_blank"
+				rel="norefferer">Qontak</a
+			>. I have been working as a backend engineer since I graduated from Brawijaya University in
+			2020 with computer engineer major. So, it's been 4 years, time does flies fast.
+		</p>
+		<p class="mt-4">
+			I'm focused on backend technology right now. Mainly use Ruby on Rails at the moment, tried a
+			lot of things, Go, Phoenix, NestJs, etc, love them. Beside web development, I also interested
+			in other fields like computer vision and IoT. Doing small projects in my spare time. My thesis
+			was about computer vision by the way, you can read <a
+				class="border-b-2 border-lime-500 text-lime-500 hover:border-lime-400 hover:text-lime-400"
+				href="https://j-ptiik.ub.ac.id/index.php/j-ptiik/article/view/7129"
+				target="_blank"
+				rel="norefferer">here</a
+			>.
+		</p>
+		<p class="mt-4">
+			I made this site to be my portfolio and blog. My writing skill needs a lot of practices...
+			That's why I'm going to share everything I want here. This will work also as my note, dont't
+			forget everything after you learn something, take notes!
+		</p>
+		<p class="mt-4">Finally, thank you for visiting this site, means a lot.</p>
+	</div>
 </div>
