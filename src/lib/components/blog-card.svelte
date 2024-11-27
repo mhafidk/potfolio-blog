647113e5a56c86b09b004ff03de432b933ffb1c9
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2024-11-27 22:25:15 +0700

    improve blog card

--- src/lib/components/blog-card.svelte
+++ src/lib/components/blog-card.svelte
@@ -2,19 +2,21 @@
 	export let post;
 </script>
 
-<div class="flex h-96 flex-col items-center gap-4 md:h-44 md:flex-row">
-	<div class="h-1/2 w-full overflow-hidden rounded md:h-full md:w-1/3">
+<div
+	class="flex h-96 flex-col items-center overflow-hidden rounded bg-lime-50 bg-opacity-50 shadow md:h-44 md:flex-row"
+>
+	<div class="h-1/2 w-full overflow-hidden md:h-full md:w-1/3">
 		<img
 			src={post.coverImage.url}
 			alt={post.title}
-			class="h-full w-full rounded object-cover transition ease-in-out hover:scale-110"
+			class="h-full w-full object-cover transition ease-in-out hover:scale-110"
 		/>
 	</div>
-	<div class="h-1/2 w-full text-center md:h-fit md:w-2/3 md:text-left">
-		<a href={`/blog/${post.slug}`} class="text-2xl font-semibold text-lime-600 hover:text-lime-700"
+	<div class="h-1/2 w-full px-4 py-4 text-center md:h-fit md:w-2/3 md:py-0 md:text-left">
+		<a href={`/blog/${post.slug}`} class="text-xl font-semibold text-lime-600 hover:text-lime-700"
 			>{post.title}</a
 		>
-		<p class="my-2 line-clamp-4 text-justify md:line-clamp-3">
+		<p class="my-2 line-clamp-4 text-justify text-base md:line-clamp-3">
 			{post.description}
 		</p>
 		<div class="flex justify-center gap-4 pt-1 font-mono text-xs text-amber-600 md:justify-start">
